90a95f76ffe033e5a54fca6ae2f19aa3313dfd52
Xavi <xavigra2004@gmail.com>
2025-11-10 18:25:16 +0100

    Test passed constructors

--- src/main/java/es/uab/tqs/parchis/model/Movimiento.java
+++ src/main/java/es/uab/tqs/parchis/model/Movimiento.java
@@ -5,7 +5,25 @@ public class Movimiento {
     private Posicion inicial;
     private Posicion destino;
     private boolean captura;
-    
+
+    public Movimiento(Posicion inicial, Posicion destino, boolean captura) {
+        this.inicial = inicial;
+        this.destino = destino;
+        this.captura = captura;
+    }
+
+    public Posicion getInicial() {
+        return inicial;
+    }
+
+    public Posicion getDestino() {
+        return destino;
+    }
+
+    public boolean isCaptura() {
+        return captura;
+    }
+
     
 
 }
--- src/test/java/es/uab/tqs/parchis/model/MovimientoTest.java
+++ src/test/java/es/uab/tqs/parchis/model/MovimientoTest.java
@@ -1,5 +1,7 @@
 package es.uab.tqs.parchis.model;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
@@ -19,4 +21,6 @@ public class MovimientoTest {
         assertEquals(mov.getDestino().getNumero(), 4);
         assertFalse(mov.isCaptura());
     }
+
+    
 }
