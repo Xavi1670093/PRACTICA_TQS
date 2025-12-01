d38ff43933fa9489a8891fd75d48f9a8b3815683
Leidy <1673565@uab.cat>
2025-12-01 16:54:54 +0100

    TestJugador2.0

--- src/main/java/es/uab/tqs/parchis/model/Jugador.java
+++ src/main/java/es/uab/tqs/parchis/model/Jugador.java
@@ -6,5 +6,34 @@ import java.util.Objects;
 
 public class Jugador{
     private final string nombre;
+    private Ficha.ColorFicha color;
+    private List<Ficha> fichas;
     private final List<Ficha> fichas;
+
+    public Jugador(String nombre, Ficha.ColorFicha color ) {
+        Objects.requireNonNull(nombre, "El nombre del jugador no puede ser null");
+        assert !nombre.isBlank() : "El nombre del jugador no puede estar vacío";
+
+        this.nombre = nombre;
+        this.color = color;
+        this.fichas = new ArrayList<>();
+ 
+        for (int i = 0; i < 4; i++) {
+            fichas.add(new Ficha(this));
+        }
+
+        assert fichas.size() == 4 : "Un jugador debe tener exactamente 4 fichas";
+    }
+
+    public String getNombre(){
+        return nombre;
+    }
+
+    public List<Ficha> getFichas() {
+        return fichas;
+    }
+
+    public boolean jugar(int numDado, Tablero tablero){
+        
+    }
 }
--- src/test/java/es/uab/tqs/parchis/model/JugadorTest.java
+++ src/test/java/es/uab/tqs/parchis/model/JugadorTest.java
@@ -12,7 +12,56 @@ class JugadorTest{
 
     @BeforeEach
     void setup() {
-        jugador = new Jugador("Leidy");
+        jugador = new Jugador("Leidy", Ficha.ColorFicha.COLOR_ROJO);
         tablero = mock(Tablero.class);
+
+        ficha1 = mock(Ficha.class);
+        ficha2 = mock(Ficha.class);
+        
+        jugador.getFichas().add(ficha1);
+        jugador.getFichas().add(ficha2);
     }
+
+    @Test
+    
+    void testJugaElegirFicha() {
+        // TEST MOVER FICHA ELEGIDA
+        when(tablero.movimientPosible(ficha1, 5)).thenReturn(true);
+        when(tablero.movimientPosible(ficha2, 5)).thenReturn(true);
+
+        String input1 = "1\n";
+        System.setIn(new ByteArrayInputStream(input1.getBytes()));
+
+        boolean resultado1 = jugador.jugar(5, tablero);
+
+        assertTrue(resultado1);
+        verify(tablero).mouFicha(ficha2, 5);
+        verify(tablero, never()).mouFicha(ficha1, 5);
+
+        // TEST NINGUNA FICHA SE PUEDE MOVER
+        when(tablero.movimientPosible(ficha1, 5)).thenReturn(false);
+        when(tablero.movimientPosible(ficha2, 5)).thenReturn(false);
+
+        boolean resultado2 = jugador.jugar(5, tablero);
+
+        assertFalse(resultado2);
+        verify(tablero, never()).mouFicha(any(), anyInt());
+
+        // TEST FICHA1 EN BARRERA
+        when(ficha1.isBarrera()).thenReturn(true);
+        when(ficha2.isBarrera()).thenReturn(false);
+
+        when(tablero.movimientPosible(ficha1, 3)).thenReturn(false);
+        when(tablero.movimientPosible(ficha2, 3)).thenReturn(true);
+
+        String input3 = "0\n";
+        System.setIn(new ByteArrayInputStream(input3.getBytes()));
+
+        boolean resultado3 = jugador.jugar(3, tablero);
+
+        assertTrue(resultado3);
+        verify(tablero).mouFicha(ficha2, 3);
+        verify(tablero, never()).mouFicha(ficha1, 3);
+    }
+
 }
