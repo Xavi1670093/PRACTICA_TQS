a62ab65a1aeb19555101ef991108e3676ef63de3
NIU1673565 <116263904+NIU1673565@users.noreply.github.com>
2025-12-07 22:23:42 +0100

    Arreglar DpC

--- src/main/java/es/uab/tqs/parchis/model/Dado.java
+++ src/main/java/es/uab/tqs/parchis/model/Dado.java
@@ -1,22 +1,36 @@
 package es.uab.tqs.parchis.model;
+
+import java.util.Objects;
 import java.util.Random;
 
 public class Dado {
+
     private static final int MIN = 1;
     private static final int MAX = 6;
-    
+
     private final Random random;
 
+    
     public Dado(Random random) {
+        Objects.requireNonNull(random, "El Random no puede ser null");
+        assert MIN < MAX;
         this.random = random;
     }
 
+   
     public Dado() {
         this(new Random());
     }
 
     public int lanzar() {
-        return random.nextInt(MAX - MIN + 1) + MIN;
+        Objects.requireNonNull(random, "El Random debe estar inicializado");
+
+        int resultado = random.nextInt(MAX - MIN + 1) + MIN;
+
+        assert resultado >= MIN && resultado <= MAX :
+            "El resultado debe estar en el rango del dado";
+
+        return resultado;
     }
 
     public int getMin() {
